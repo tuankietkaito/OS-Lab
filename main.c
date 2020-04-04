6dd0a0e546495b2ae94a189144ebdbc3784522f8
Tuấn Kiệt Mai Huỳnh <tuankietkaito@gmail.com>
2020-04-04 23:36:17 +0700

    final

--- main.c
+++ main.c
@@ -35,18 +35,22 @@ int main(int argc, char *argv[])
             sub[z] = '\0';
         }
     }
-    printf("argc = %d\n", argc);
+    /*printf("argc = %d\n", argc);
     for (int i = 0; i < argc; i++)
         printf("--> %s\n", argv[i]);
-    printf("file = %s, substr = %s\n", argv[1], sub);
+    printf("file = %s, substr = %s\n", argv[1], sub);*/
 
     FILE *fp = fopen(argv[1], "r");
     char buff[500];
     int line = 1;
     while (fgets(buff, sizeof(buff), fp) != NULL)
     {
-        printf("Line %d: read_line = %d - %s", line, read_line(buff), buff);
-        printf("==> Find = %d\n", find_sub_string(buff, sub));
+        //printf("Line %d: read_line = %d - %s", line, read_line(buff), buff);
+        //printf("==> Find = %d\n", find_sub_string(buff, sub));
+        if (find_sub_string(buff, sub) != -1)
+        {
+            printf("Line %d: %s", line, buff);
+        }
         line++;
     }
     fclose(fp);
